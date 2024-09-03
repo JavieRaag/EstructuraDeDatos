8856f238a549bf86cf58a3439a37be9788104e1a
JavieRaag <144748730+JavieRaag@users.noreply.github.com>
2024-09-03 12:29:57 -0600

    Corrección ListaLigada

--- Tarea4/listaLigada/listaLigada.java
+++ Tarea4/listaLigada/listaLigada.java
@@ -1,19 +1,15 @@
 package listaLigada;
 
-public class listaLigada<T> {
-    private NodoDoble<T> head;
-    private NodoDoble<T> tail;
+public class ListaLigada<T> {
+    private Nodo<T> head;
+    private Nodo<T> tail;
     private int tamanio;
 
-    public listaLigada() {
+    public ListaLigada() {
     }
 
     public boolean estaVacia() {
-        boolean res = false;
-        if (this.head == null && this.tail == null) {
-            res = true;
-        }
-        return res;
+        return this.head == null;
     }
 
     public int getTamanio() {
@@ -21,12 +17,11 @@ public class listaLigada<T> {
     }
 
     public void agregarAlInicio(T valor) {
-        NodoDoble<T> nuevo = new NodoDoble<>(valor);
+        Nodo<T> nuevo = new Nodo<>(valor);
         if (this.estaVacia()) {
             this.head = nuevo;
             this.tail = nuevo;
         } else {
-            this.head.setAnterior(nuevo);
             nuevo.setSiguiente(this.head);
             this.head = nuevo;
         }
@@ -34,107 +29,112 @@ public class listaLigada<T> {
     }
 
     public void agregarAlFinal(T valor) {
-        NodoDoble<T> nuevo = new NodoDoble<>(valor);
+        Nodo<T> nuevo = new Nodo<>(valor);
         if (this.estaVacia()) {
             this.head = nuevo;
             this.tail = nuevo;
         } else {
-            NodoDoble<T> aux = this.head;
-            while (aux.getSiguiente() != null) {
-                aux = aux.getSiguiente();
-            }
-            aux.setSiguiente(nuevo);
-            nuevo.setAnterior(aux);
+            this.tail.setSiguiente(nuevo);
             this.tail = nuevo;
         }
         this.tamanio++;
     }
 
-    /**
-     * @param direccion 0 --> izq a derecha  si es 1 --> derecha a izq
-     */
-    public void transversal(int direccion) {
-        if (direccion == 1) {
-            NodoDoble<T> aux = this.tail;
-            while (aux != null) {
-                System.out.print(aux);
-                aux = aux.getAnterior();
-            }
-        } else {
-            NodoDoble<T> aux = this.head;
-            while (aux != null) {
-                System.out.print(aux);
-                aux = aux.getSiguiente();
-            }
+    public void transversal() {
+        Nodo<T> aux = this.head;
+        while (aux != null) {
+            System.out.print(aux);
+            aux = aux.getSiguiente();
         }
-        System.out.println("");
+        System.out.println();
     }
 
     public void agregarDespuesDe(T referencia, T valor) {
-        NodoDoble<T> aux = this.head;
-        while (aux.getData() != referencia){
+        Nodo<T> aux = this.head;
+        while (aux != null && !aux.getData().equals(referencia)) {
             aux = aux.getSiguiente();
         }
-        if(aux == null){
+        if (aux == null) {
             System.out.println("No existe la referencia!!!");
-        }else{
-
-
+        } else {
+            Nodo<T> nuevo = new Nodo<>(valor, aux.getSiguiente());
+            aux.setSiguiente(nuevo);
+            if (aux == this.tail) {
+                this.tail = nuevo;
+            }
+            this.tamanio++;
         }
-
     }
 
     public void eliminarPrimero() {
-        if (this.head == this.tail) {
-            this.head = null;
-            this.tail = null;
-        } else {
-
+        if (!this.estaVacia()) {
             this.head = this.head.getSiguiente();
-            this.head.setAnterior(null);
+            if (this.head == null) {
+                this.tail = null;
+            }
+            this.tamanio--;
         }
-        this.tamanio--;
     }
 
     public void eliminarFinal() {
-        if (this.head == this.tail) {
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.tail = this.tail.getAnterior();
-            this.tail.setSiguiente(null);
+        if (!this.estaVacia()) {
+            if (this.head == this.tail) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                Nodo<T> aux = this.head;
+                while (aux.getSiguiente() != this.tail) {
+                    aux = aux.getSiguiente();
+                }
+                aux.setSiguiente(null);
+                this.tail = aux;
+            }
+            this.tamanio--;
         }
-        this.tamanio--;
     }
 
     public void eliminarPosicion(int posicion) {
-        NodoDoble<T> aux = this.head;
-        for (int i = 1; i < posicion; i++) {
-            aux = aux.getSiguiente();
+        if (posicion < 1 || posicion > this.tamanio) {
+            System.out.println("Posición fuera de rango.");
+            return;
         }
-        aux.getAnterior().setSiguiente(aux.getSiguiente());
-        aux.getSiguiente().setAnterior(aux.getAnterior());
-        this.tamanio--;
+        if (posicion == 1) {
+            eliminarPrimero();
+        } else {
+            Nodo<T> aux = this.head;
+            for (int i = 1; i < posicion - 1; i++) {
+                aux = aux.getSiguiente();
+            }
+            aux.setSiguiente(aux.getSiguiente().getSiguiente());
+            if (aux.getSiguiente() == null) {
+                this.tail = aux;
+            }
+            this.tamanio--;
+        }
     }
 
     public int buscarValor(T valor) {
-        NodoDoble<T> aux = this.head;
+        Nodo<T> aux = this.head;
         int posicion = 1;
 
         while (aux != null) {
             if (aux.getData().equals(valor)) {
-                System.out.println("la posicion de "+ valor + " es: "+ posicion);
+                System.out.println("La posición de " + valor + " es: " + posicion);
+                return posicion;
             }
             aux = aux.getSiguiente();
             posicion++;
         }
 
-        return -1;
+        return -1; // No encontrado
     }
 
     public void actualizarValor(int posicion, T valor) {
-
-        NodoDoble<T> aux = this.head;
+        if (posicion < 1 || posicion > this.tamanio) {
+            System.out.println("Posición fuera de rango.");
+            return;
+        }
+        Nodo<T> aux = this.head;
         int contador = 1;
 
         while (contador < posicion) {
@@ -143,5 +143,4 @@ public class listaLigada<T> {
         }
         aux.setData(valor);
     }
-
 }
